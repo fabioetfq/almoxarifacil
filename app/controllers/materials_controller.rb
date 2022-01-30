ed93fd80049d539d79e4fc873e5b85a25353d071
Fabio Oliveira <fabio.oliveira@agricultura.gov.br>
2022-01-30 20:50:41 -0300

    Materials and Transactions routes

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,2 +1,14 @@
 class MaterialsController < ApplicationController
+  before_action :find, only: [:show]
+  def index
+    @materials = Material.all
+  end
+
+  def show
+  end
+
+  private
+  def find
+    @material = Material.find(params[:id])
+  end
 end
