a0883e73424b455aa2c57c660350d941dbefcdb4
Fabio Oliveira <fabio.oliveira@agricultura.gov.br>
2022-01-31 00:57:30 -0300

    Materials controller and views

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,15 +1,44 @@
 class MaterialsController < ApplicationController
-  before_action :find, only: [:show]
+  before_action :find, only: [:show, :edit, :update, :destroy]
+  
   def index
     @materials = Material.all
   end
 
-  def show
+  def show; end
+
+  def new
+    @material = Material.new
+  end
+
+  def create
+    @material = Material.new(material_params)
+    if @material.save!
+      redirect_to material_path(@material)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @material.update(material_params)
+    redirect_to material_path(@material)
+  end
+
+  def destroy
+    @material.destroy
+    redirect_to materials_path
   end
 
   private
-  
+
   def find
     @material = Material.find(params[:id])
+  end
+
+  def material_params
+    params.require(:material).permit(:name, :category, :qty)
   end
 end
